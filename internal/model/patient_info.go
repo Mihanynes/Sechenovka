c193f890faa95025de0f63dbb68aa885e327e0ba
Mikhail Petrov <mapetrov@avito.ru>
2024-12-21 17:14:42 +0300

    snake

--- internal/model/patient_info.go
+++ internal/model/patient_info.go
@@ -1,9 +1,9 @@
 package model
 
 type PatientInfo struct {
-	FirstName  string
-	MiddleName string
-	LastName   string
-	Snils      string
-	Email      string
+	FirstName  string `json:"first_name"`
+	MiddleName string `json:"middle_name"`
+	LastName   string `json:"last_name"`
+	Snils      string `json:"snils"`
+	Email      string `json:"email"`
 }
