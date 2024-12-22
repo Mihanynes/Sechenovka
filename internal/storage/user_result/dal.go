c4fdd3c368260a612c0003b8dcd763fccc3779e4
Mikhail Petrov <mapetrov@avito.ru>
2024-12-22 15:03:40 +0300

    maybebaybe

--- internal/storage/user_result/dal.go
+++ internal/storage/user_result/dal.go
@@ -6,6 +6,6 @@ type UserResult struct {
 	gorm.Model
 	UserId     string `gorm:"index;not null"` // Идентификатор пользователя
 	PassNum    int    `gorm:"type:int;not null"`
-	TotalScore int    `gorm:"type:int;not null"`
+	TotalScore int    `gorm:"type:int;"`
 	IsFailed   bool   `gorm:"type:boolean;not null"` //true - хорошо, false - плох
 }
